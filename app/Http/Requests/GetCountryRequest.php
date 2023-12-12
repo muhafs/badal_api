27205d4bddd3e735c600b0b937f6514366aa684d
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-12 16:11:07 +0700

    --ft : add country id request

---
+++ app/Http/Requests/GetCountryRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class GetCountryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'id' => $this->route('id'),
+        ]);
+    }
+
+    public function rules()
+    {
+        return ['id' => 'required|numeric|exists:countries,id'];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'Country is missing',
+            'numeric' => 'Country is invalid',
+            'exists' => 'No Country found'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json(
+                [
+                    'message' => $validator->errors()->first(),
+                ],
+                404
+            )
+        );
+    }
+}
