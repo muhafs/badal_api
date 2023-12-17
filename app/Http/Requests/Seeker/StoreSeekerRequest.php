b1f1d5c6d28ebe026865e8b66bcb1d07172fe9b9
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-17 12:11:04 +0700

    --ft : add seeker store validation message api

--- app/Http/Requests/Seeker/StoreSeekerRequest.php
+++ app/Http/Requests/Seeker/StoreSeekerRequest.php
@@ -18,46 +18,70 @@ class StoreSeekerRequest extends FormRequest
     public function rules()
     {
         return [
-            "first_name" => 'required|string',
-            "last_name" => 'nullable|string',
-            "gender" => 'required|string|max:1|in:M,F,m,f',
-            "birth_date" => 'nullable|date',
-            "nationality_id" => 'required|numeric|exists:cities,id',
+            "first_name" => "required|string",
+            "last_name" => "nullable|string",
+            "gender" => "required|string|max:1|in:M,F,m,f",
+            "birth_date" => "nullable|date",
+            "nationality_id" => "required|numeric|exists:cities,id",
 
-            "hajj_name" => 'nullable|string|max:32',
-            "currency" => 'required|string|max:3',
-            "price" => 'required|integer',
+            "hajj_name" => "nullable|string|max:32",
+            "currency" => "required|string|max:3",
+            "price" => "required|integer",
 
-            "email" => 'nullable|email|unique:contacts,email',
-            "phone_code" => 'required|numeric',
-            "phone_number" => 'required|numeric|unique:contacts,phone_number',
-            "whatsapp" => 'nullable|numeric|unique:contacts,whatsapp',
-            "instagram" => 'nullable|string|unique:contacts,instagram',
-            "facebook" => 'nullable|string|unique:contacts,facebook',
+            "email" => "nullable|email|unique:contacts,email",
+            "phone_code" => "required|numeric",
+            "phone_number" => "required|numeric|unique:contacts,phone_number",
 
-            "address" => 'required|string',
-            "postcode" => 'nullable|string|max:10',
-            "city_id" => 'required|numeric|exists:cities,id',
+            "whatsapp" => "nullable|numeric|unique:contacts,whatsapp",
+            "instagram" => "nullable|string|unique:contacts,instagram",
+            "facebook" => "nullable|string|unique:contacts,facebook",
+
+            "address" => "required|string",
+            "postcode" => "nullable|string|max:10",
+            "city_id" => "required|numeric|exists:cities,id",
         ];
     }
 
-    //TODO: has to be done
     public function messages()
     {
         return [
-            'name.required' => 'Country Name can\'t be Empty',
-            'name.unique' => 'This name has already taken',
+            "first_name.required" => "Seeker Name can't be Empty",
 
-            'nationality.required' => 'Nationality can\'t be Empty',
-            'nationality.unique' => 'this nationality has already taken',
+            "gender.required" => "Gender is missing",
+            "gender.*" => "Gender is invalid",
 
-            'phone_code.required' => 'Phone code can\'t be Empty',
-            'phone_code.numeric' => 'Phone code must be a number',
-            'phone_code.unique' => 'this Phone code has already taken',
+            "nationality_id.required" => "Nationality is missing",
+            "nationality_id.*" => "Nationality is invalid",
 
-            'currency_code.required' => 'Currency code can\'t be Empty',
-            'currency_code.max' => 'Currency code has to be 3 Characters',
-            'currency_code.unique' => 'this Currency code has already taken',
+            "hajj_name.max" => "Hajj name is too long, max is 32 characters",
+
+            "currency.required" => "Currency is missing",
+            "currency.*" => "Currency is invalid",
+
+            "price.integer" => "Price can't be Empty",
+            "price.integer" => "Price must be a number",
+
+            "email.email" => "Email is invalid",
+            "email.unique" => "this Email has already taken",
+
+            "phone_code.required" => "Phone code is missing",
+            "phone_code.numeric" => "Phone code must be a number",
+
+            "phone_number.required" => "Phone number can't be Empty",
+            "phone_number.numeric" => "Phone number must be a number",
+            "phone_number.unique" => "this Phone number has already taken",
+
+            "whatsapp.numeric" => "Whatsapp account must be a number",
+            "whatsapp.unique" => "this Whatsapp account has already taken",
+
+            "instagram.unique" => "this Instagram User has already taken",
+            "facebook.unique" => "this Facebook User has already taken",
+
+            "address.required" => "Address can't be Empty",
+            "postcode.max" => "Post Code is too long, max 10 digits",
+
+            "city_id.required" => "City is missing",
+            "city_id.*" => "no City found",
         ];
     }
 
