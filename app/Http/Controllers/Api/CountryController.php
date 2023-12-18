5225f2d6f603fe554b8dc9a65e2d85c05be3b32d
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-18 11:54:06 +0700

    --cg : add the rest controllers for each model

--- app/Http/Controllers/Api/CountryController.php
+++ app/Http/Controllers/Api/CountryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Country;
 use App\Models\Nationality;
+use App\Http\Requests\PostCaller;
 use App\Http\Controllers\Controller;
 use App\Http\Traits\HasJsonResponse;
 use App\Http\Resources\Country\CountryResource;
@@ -13,6 +14,7 @@ use App\Http\Requests\Country\UpdateCountryRequest;
 use App\Http\Resources\Country\CountryListResource;
 use App\Http\Resources\Country\CurrencyListResource;
 use App\Http\Resources\Country\phoneCodeListResource;
+use App\Http\Requests\Nationality\StoreNationalityRequest;
 
 class CountryController extends Controller
 {
@@ -27,7 +29,7 @@ class CountryController extends Controller
 
     function show(GetCountryRequest $request)
     {
-        $country = new CountryResource(Country::with('nationality')->find($request->id));
+        $country = new CountryResource(Country::find($request->id));
 
         return $this->jsonResponse("Success", $country);
     }
@@ -36,22 +38,23 @@ class CountryController extends Controller
     {
         $country = Country::create([
             "name" => str($request->name)->title()->squish(),
-            "currency_code" => str($request->currency_code)->upper()->trim(),
-            "phone_code" => ($request->phone_code),
+            "short_code" => str($request->short_code)->upper()->trim(),
+            "long_code" => str($request->long_code)->upper()->trim(),
         ]);
 
         if (!$country) {
             $this->throwResponse("Something went Error while Creating Country");
         }
 
-        $nationality = Nationality::create([
-            "name" => str($request->nationality)->title()->squish(),
-            "country_id" => $country->id
-        ]);
-
-        if (!$nationality) {
-            $this->throwResponse("Something went Error while Creating Country's Nationality");
-        }
+        (new PostCaller(
+            NationalityController::class,
+            "store",
+            StoreNationalityRequest::class,
+            [
+                "name" => $request->nationality_name,
+                "country_id" => $country->id,
+            ]
+        ))->call();
 
         return $this->jsonResponse("Success Create Country", $country, 201);
     }
@@ -75,28 +78,12 @@ class CountryController extends Controller
 
     function destroy(GetCountryRequest $request)
     {
-        $country = Country::find($request->id);
-
-        $country->delete();
+        $country = Country::destroy($request->id);
 
         if (!$country) {
             $this->throwResponse("Something went Error while Deleting Country");
         }
 
         return $this->jsonResponse("Success Delete Country", $country, 201);
-    }
-
-    function currencies()
-    {
-        $currencies = CurrencyListResource::collection(Country::all());
-
-        return $this->jsonResponse("Success", $currencies);
-    }
-
-    function phoneCodes()
-    {
-        $phoneCodes = phoneCodeListResource::collection(Country::all());
-
-        return $this->jsonResponse("Success", $phoneCodes);
     }
 }
---
+++ app/Http/Controllers/Api/CurrencyController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Currency;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCurrencyRequest;
+use App\Http\Requests\UpdateCurrencyRequest;
+
+class CurrencyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCurrencyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Currency $currency)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCurrencyRequest $request, Currency $currency)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Currency $currency)
+    {
+        //
+    }
+}
