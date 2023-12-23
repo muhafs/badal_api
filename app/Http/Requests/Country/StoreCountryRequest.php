38e20778828ab4dd331a1d3421d294529f9f36e0
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-23 12:04:28 +0700

    --ft : edit request api of country by adding its detail info validations

--- app/Http/Requests/Country/StoreCountryRequest.php
+++ app/Http/Requests/Country/StoreCountryRequest.php
@@ -18,25 +18,52 @@ class StoreCountryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|unique:countries,name',
-            'short_code' => 'required|string|unique:countries,short_code|min:2|max:2',
-            'long_code' => 'required|string|unique:countries,long_code|min:3|max:3',
+            //? Country Validation
+            "name" => "required|string|unique:countries,name",
+            "short_code" => "required|string|unique:countries,short_code|min:2|max:2",
+            "long_code" => "required|string|unique:countries,long_code|min:3|max:3",
+
+            //? Nationality Validation
+            "nationality_name"          => "required|string|unique:nationalities,name",
+
+            //? Currency Validation
+            "currency_name"          => "required|string|unique:currencies,name",
+            "currency_code"          => "required|string|unique:currencies,code",
+
+            //? Phone Validation
+            "phone_code"          => "required|numeric|unique:phones,code"
         ];
     }
 
     public function messages()
     {
         return [
-            'name.required' => 'Country Name can\'t be Empty',
-            'name.unique' => 'This name has already taken',
+            //? Country Validator Messages
+            "name.required" => "Country Name can't be Empty",
+            "name.unique" => "This name has already taken",
 
-            'short_code.required' => 'Short code can\'t be Empty',
-            'short_code.unique' => 'this Short code has already taken',
-            'short_code.*' => 'Short code should be 2 characters',
+            "short_code.required" => "Short code can't be Empty",
+            "short_code.unique" => "this Short code has already taken",
+            "short_code.*" => "Short code should be 2 characters",
 
-            'long_code.required' => 'Long code can\'t be Empty',
-            'long_code.unique' => 'this Long code has already taken',
-            'long_code.*' => 'Long code should be 3 characters'
+            "long_code.required" => "Long code can't be Empty",
+            "long_code.unique" => "this Long code has already taken",
+            "long_code.*" => "Long code should be 3 characters",
+
+            //? Nationality Validator Messages
+            "nationality_name.required" => "Nationality Name can't be Empty",
+            "nationality_name.unique" => "This name has already taken",
+
+            //? Currency Validator Messages
+            "currency_name.required" => "Currency Name can't be Empty",
+            "currency_name.unique" => "This name has already taken",
+
+            "currency_code.required" => "Currency Code can't be Empty",
+            "currency_code.unique" => "This code has already taken",
+
+            //? Phone Validator Messages
+            "phone_code.required" => "phone Code can't be Empty",
+            "phone_code.unique" => "This code has already taken"
         ];
     }
 
--- app/Http/Requests/Country/UpdateCountryRequest.php
+++ app/Http/Requests/Country/UpdateCountryRequest.php
@@ -18,37 +18,64 @@ class UpdateCountryRequest extends FormRequest
     protected function prepareForValidation()
     {
         $this->merge([
-            'id' => $this->route('country'),
+            "id" => $this->route("country"),
         ]);
     }
 
     public function rules(): array
     {
         return [
-            'id' => 'required|numeric|exists:countries,id',
-            'name' => 'required|string|unique:countries,name,' . $this->id,
-            'short_code' => 'required|string|min:2|max:2|unique:countries,short_code,' . $this->id,
-            'long_code' => 'required|string|min:3|max:3|unique:countries,long_code,' . $this->id,
+            //? Country Validation
+            "id" => "required|numeric|exists:countries,id",
+            "name" => "required|string|unique:countries,name," . $this->id,
+            "short_code" => "required|string|min:2|max:2|unique:countries,short_code," . $this->id,
+            "long_code" => "required|string|min:3|max:3|unique:countries,long_code," . $this->id,
+
+            //? Nationality Validation
+            "nationality_name" => "required|string|unique:nationalities,name," . $this->id,
+
+            //? Currency Validation
+            "currency_name" => "required|string|unique:currencies,name," . $this->id,
+            "currency_code" => "required|string|unique:currencies,code," . $this->id,
+
+            //? Phone Validation
+            "phone_code" => "required|string|unique:phones,code," . $this->id
         ];
     }
 
     public function messages()
     {
         return [
-            'id.required' => 'Country is missing',
-            'id.numeric' => 'Country is invalid',
-            'id.exists' => 'No Country found',
+            //? Country Validator Message
+            "id.required" => "Country is missing",
+            "id.numeric" => "Country is invalid",
+            "id.exists" => "No Country found",
 
-            'name.required' => 'Country Name can\'t be Empty',
-            'name.unique' => 'This name has already taken',
+            "name.required" => "Country Name can't be Empty",
+            "name.unique" => "This name has already taken",
 
-            'short_code.required' => 'Short code can\'t be Empty',
-            'short_code.unique' => 'this Short code has already taken',
-            'short_code.*' => 'Short code should be 2 characters',
+            "short_code.required" => "Short code can't be Empty",
+            "short_code.unique" => "this Short code has already taken",
+            "short_code.*" => "Short code should be 2 characters",
 
-            'long_code.required' => 'Long code can\'t be Empty',
-            'long_code.unique' => 'this Long code has already taken',
-            'long_code.*' => 'Long code should be 3 characters'
+            "long_code.required" => "Long code can't be Empty",
+            "long_code.unique" => "this Long code has already taken",
+            "long_code.*" => "Long code should be 3 characters",
+
+            //? Nationality Validator Message
+            "nationality_name.required" => "Nationality Name can't be Empty",
+            "nationality_name.unique" => "This name has already taken",
+
+            //? Currency Validator Message
+            "currency_name.required" => "Currency Name can't be Empty",
+            "currency_name.unique" => "This name has already taken",
+
+            "currency_code.required" => "Currency Code can't be Empty",
+            "currency_code.unique" => "This code has already taken",
+
+            //? Phone Validator Message
+            "phone_code.required" => "Phone Code can't be Empty",
+            "phone_code.unique" => "This code has already taken"
         ];
     }
 
