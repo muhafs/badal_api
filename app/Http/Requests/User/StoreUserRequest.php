f59f40db20fb0d8da9af496e9940034de51e294d
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-23 12:04:11 +0700

    --ft : edit request api of user by adding its detail info validations

--- app/Http/Requests/User/StoreUserRequest.php
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -18,28 +18,92 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name'          => 'required|string',
-            'last_name'          => 'nullable|string',
-            'gender'          => 'required|string|max:1|in:M,F,m,f',
-            'birth_date'          => 'nullable|date',
-            'image'          => 'nullable|image|max:2048',
-            'type'          => 'required|string|in:SKR,PRF,ADM',
-            'nationality_id'    => 'required|numeric|exists:nationalities,id'
+            //? User
+            "first_name"          => "required|string",
+            "last_name"          => "nullable|string",
+            "gender"          => "required|string|max:1|in:M,F,m,f",
+            "birth_date"          => "nullable|date",
+            "image"          => "nullable|image|max:2048",
+            "type"          => "required|string|in:SKR,PRF,ADM",
+            "nationality_id"    => "required|numeric|exists:nationalities,id",
+
+            //? Seeker
+            "hajj_name" => "nullable|string|max:32",
+            "type" => "required|string|in:HAJJ,UMRAH",
+            "price" => "required_if:type,SKR|integer",
+            "currency_id" => "required_if:type,SKR|numeric|exists:currencies,id",
+
+            //? Performer
+            "nickname" => "nullable|string|max:32",
+            "bio" => "nullable|string",
+
+            //? Address
+            "address" => "required|string",
+            "postcode" => "required|string|max:10",
+            "city_id" => "required|numeric|exists:cities,id",
+
+            //? Contact
+            "phone_number" => "required|numeric|unique:contacts,phone_number",
+            "email" => "nullable|email|unique:contacts,email",
+            "whatsapp" => "nullable|numeric|unique:contacts,whatsapp",
+            "instagram" => "nullable|string|unique:contacts,instagram",
+            "facebook" => "nullable|string",
+            "phone_code_id" => "required|numeric|exists:phones,id"
         ];
     }
 
     public function messages()
     {
         return [
-            'first_name.required' => 'First Name can\'t be Empty',
+            //? User
+            "first_name.required" => "First Name can't be Empty",
+            "gender.*" => "Gender is invalid",
+            "birth_date.*" => "Birth Date is invalid",
+            "image.max" => "Image is too large, max 2MB",
+            "type.*" => "Type is is invalid",
 
-            'gender.*' => 'Gender is invalid',
-            'birth_date.*' => 'Birth Date is invalid',
-            'image.max' => 'Image is too large, max 2MB',
-            'type.*' => 'Type is is invalid',
+            "nationality_id.required" => "Nationality is missing",
+            "nationality_id.exsits" => "no Nationality found",
 
-            'nationality_id.required' => 'Nationality is missing',
-            'nationality_id.exsits' => 'no Nationality found'
+            //? Seeker
+            "hajj_name.*" => "Hajj name too long, max is 32 characters",
+
+            "seeker_type.required" => "Type is missing",
+            "seeker_type.*" => "Type is invalid",
+
+            "price.required_if" => "Price can't be Empty",
+            "price.integer" => "Price must be a number",
+
+            "currency_id.required" => "Currency is missing",
+            "currency_id.*" => "no Currency found",
+
+            //? Performer
+            "nickname.*" => "Nickname too long, max is 32 characters",
+
+            //? Address
+            "address.required" => "Address Name can't be Empty",
+
+            "postcode.required" => "Postal Code can't be Empty",
+            "postcode.max" => "Postal Code maximal 10 Digits",
+
+            "city_id.required" => "City is missing",
+            "city_id.exists" => "no City found",
+
+            //? Contact
+            "phone_number.required" => "Phone Number can't be Empty",
+            "phone_number.numeric" => "Phone Number invalid",
+            "phone_number.unique" => "Phone Number has already taken",
+
+            "email.email" => "Email is invalid",
+            "email.unique" => "this Email has already taken",
+
+            "whatsapp.numeric" => "WhatsApp Number invalid",
+            "whatsapp.unique" => "this WhatsApp Number has already taken",
+
+            "instagram.unique" => "Instagram has already taken",
+
+            "phone_code_id.required" => "Phone Code is missing",
+            "phone_code_id.exsits" => "no Phone Code found"
         ];
     }
 
--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -18,40 +18,104 @@ class UpdateUserRequest extends FormRequest
     protected function prepareForValidation()
     {
         $this->merge([
-            'id' => $this->route('user'),
+            "id" => $this->route("user"),
         ]);
     }
 
     public function rules(): array
     {
         return [
-            'id'          => 'required|numeric|exists:users,id',
-            'first_name'          => 'required|string',
-            'last_name'          => 'nullable|string',
-            'gender'          => 'required|string|max:1|in:M,F,m,f',
-            'birth_date'          => 'nullable|date',
-            'image'          => 'nullable|image|max:2048',
-            'type'          => 'required|string|in:SKR,PRF,ADM',
-            'nationality_id'    => 'required|numeric|exists:nationalities,id'
+            //? User
+            "id"          => "required|numeric|exists:users,id",
+            "first_name"          => "required|string",
+            "last_name"          => "nullable|string",
+            "gender"          => "required|string|max:1|in:M,F,m,f",
+            "birth_date"          => "nullable|date",
+            "image"          => "nullable|image|max:2048",
+            "type"          => "required|string|in:SKR,PRF,ADM",
+            "nationality_id"    => "required|numeric|exists:nationalities,id",
+
+            //? Seeker
+            "hajj_name" => "nullable|string|max:32",
+            "type" => "required|string|in:HAJJ,UMRAH",
+            "price" => "required_if:type,SKR|integer",
+            "currency_id" => "required_if:type,SKR|numeric|exists:currencies,id",
+
+            //? Performer
+            "nickname" => "nullable|string|max:32",
+            "bio" => "nullable|string",
+
+            //? Address
+            "address" => "required|string",
+            "postcode" => "required|string|max:10",
+            "city_id" => "required|numeric|exists:cities,id",
+
+            //? Contact
+            "phone_number" => "required|numeric|unique:contacts,phone_number," . $this->id,
+            "email" => "nullable|email|unique:contacts,email," . $this->id,
+            "whatsapp" => "nullable|numeric|unique:contacts,whatsapp," . $this->id,
+            "instagram" => "nullable|string|unique:contacts,instagram," . $this->id,
+            "facebook" => "nullable|string",
+            "phone_code_id" => "required|numeric|exists:phones,id"
         ];
     }
 
     public function messages()
     {
         return [
-            'id.required' => 'User is missing',
-            'id.numeric' => 'User is invalid',
-            'id.exists' => 'No User found',
+            //? User
+            "id.required" => "User is missing",
+            "id.numeric" => "User is invalid",
+            "id.exists" => "No User found",
 
-            'first_name.required' => 'First Name can\'t be Empty',
+            "first_name.required" => "First Name can't be Empty",
+            "gender.*" => "Gender is invalid",
+            "birth_date.*" => "Birth Date is invalid",
+            "image.max" => "Image is too large, max 2MB",
+            "type.*" => "Type is is invalid",
 
-            'gender.*' => 'Gender is invalid',
-            'birth_date.*' => 'Birth Date is invalid',
-            'image.max' => 'Image is too large, max 2MB',
-            'type.*' => 'Type is is invalid',
+            "nationality_id.required" => "Nationality is missing",
+            "nationality_id.exsits" => "no Nationality found",
 
-            'nationality_id.required' => 'Nationality is missing',
-            'nationality_id.exsits' => 'no Nationality found'
+            //? Seeker
+            "hajj_name.*" => "Hajj name too long, max is 32 characters",
+
+            "seeker_type.required" => "Type is missing",
+            "seeker_type.*" => "Type is invalid",
+
+            "price.required_if" => "Price can't be Empty",
+            "price.integer" => "Price must be a number",
+
+            "currency_id.required" => "Currency is missing",
+            "currency_id.*" => "no Currency found",
+
+            //? Performer
+            "nickname.*" => "Nickname too long, max is 32 characters",
+
+            //? Address
+            "address.required" => "Address Name can't be Empty",
+
+            "postcode.required" => "Postal Code can't be Empty",
+            "postcode.max" => "Postal Code maximal 10 Digits",
+
+            "city_id.required" => "City is missing",
+            "city_id.exists" => "no City found",
+
+            //? Contact
+            "phone_number.required" => "Phone Number can't be Empty",
+            "phone_number.numeric" => "Phone Number invalid",
+            "phone_number.unique" => "Phone Number has already taken",
+
+            "email.email" => "Email is invalid",
+            "email.unique" => "this Email has already taken",
+
+            "whatsapp.numeric" => "WhatsApp Number invalid",
+            "whatsapp.unique" => "this WhatsApp Number has already taken",
+
+            "instagram.unique" => "Instagram has already taken",
+
+            "phone_code_id.required" => "Phone Code is missing",
+            "phone_code_id.exsits" => "no Phone Code found"
         ];
     }
 
